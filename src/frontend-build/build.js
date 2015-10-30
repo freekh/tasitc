6919c0c3959b6e1687731874ac030fe28564f8a3
Fredrik Ekholdt <freekh@gmail.com>
2015-10-30 16:24:35 +0100

    some html experiements

--- src/frontend-build/build.js
+++ src/frontend-build/build.js
@@ -1,15 +1,24 @@
+'use strict'
+
 const babelify = require('babelify')
 const browserifyInc = require('browserify-incremental')
 const browserify = require('browserify')
 const xtend = require('xtend')
 const env = require('../shared/env')
 const path = require('path')
+const fs = require('fs')
+
+if (!fs.statSync(env.tmpDir).isDirectory()) {
+  fs.mkdirSync(env.tmpDir)
+}
 
 module.exports = (stream, incremental, options) => {
   const frontendDir = path.resolve(env.appDir + '/src/frontend')
 
-  const b = browserify(
-    xtend(browserifyInc.args, options || {})
+  let b = browserify(
+    xtend(browserifyInc.args, options || {
+      debug: true
+    })
   ).transform(babelify.configure({
     sourceMapRelative: frontendDir
   }))
@@ -21,12 +30,15 @@ module.exports = (stream, incremental, options) => {
   }
 
   b.add(frontendDir + '/app.js')
+
   const bundle = b.bundle()
 
   bundle.on('error', (err) => {
     stream.emit('error', err)
   })
+
   bundle.pipe(stream)
+
   bundle.on('end', () => {
     stream.emit('end')
   })
--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,3 +1,76 @@
+'use strict'
+
+const document = require('global/document')
 const hg = require('mercury')
+const xtend = require('xtend')
+const h = hg.h
+1
+let App = () => {
+  return hg.state({})
+}
+
+let Header = {}
+Header.style = {
+  display: 'flex',
+  'min-height': '1000px',
+  'max-height': '1000px',
+  'width': '100%',
+  'height': 'auto',
+  'position': 'fixed',
+  'background-size': 'cover',
+  'top': '0',
+  'left': '0',
+  'background': 'url(https://src.responsive.io/w=2000/https://tictail.com/cms/wp-content/uploads/2015/05/Tictail_Header._3.jpg) no-repeat center center fixed'
+}
+
+Header.text = {}
+Header.text.style = {
+  display: 'flex',
+  'position': 'absolute',
+  'color': 'white',
+  'top': '200px',
+  'left': '200px',
+  '-webkit-transform': 'translate(-50%, 50%)',
+  '-ms-transform': 'translate(-50%, 50%)',
+  'transform': 'translate(-50%, 50%)',
+  'z-index': '1'
+}
+
+Header.text.render = (state) => {
+  return h(
+    'h1', {
+      style: Header.text.style
+    }, String('hei')
+  )
+}
 
 
+Header.render = (state) => {
+  return h('div.row', {
+    style: Header.style
+  }, [
+    Header.text.render(state)
+  ])
+}
+
+
+App.render = (state) => {
+  return h('div.container', {
+    style: {
+      display: 'flex',
+      'flex-direction': 'column'
+    }
+  },[
+    Header.render(state),
+    h('div.row', {
+      style: {
+        display: 'flex',
+        position: 'absolute',
+        top: '100%'
+        
+      }
+    }, String('heai'))
+  ])
+}
+
+hg.app(document.body, App(), App.render)

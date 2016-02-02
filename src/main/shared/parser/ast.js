b7951a29aecc7d4e0883c74db90544f633d2d10e
Fredrik Ekholdt <freekh@gmail.com>
2016-02-02 22:00:08 +0100

    Some things work, but not really

--- src/main/shared/parser/ast.js
+++ src/main/shared/parser/ast.js
@@ -1,8 +1,84 @@
+'use strict'
+
 const P = require('parsimmon')
+
+const regex = P.regex
+const string = P.string
+const optWhitespace = P.optWhitespace
+const lazy = P.lazy
+const alt = P.alt
+
+const lparen = string('(')
+const rparen = string(')')
+const comment = P.string('#').then(P.takeWhile(l => {
+  return l !== '\n'
+}))
+
+const ignore = P.whitespace.or(comment.many())
+const lexeme = p => p.skip(optWhitespace)
+
+const id = lexeme(regex(/[a-zA-Z_][a-zA-z0-9_]*/))
+const exprEnd = P.string('\\n', '\n').or(P.eof)
+const params = lparen
+
+const p1 = id.map(v => {
+  return { value: v, type: 'command' }
+}).skip(P.eof)
+
+const p2 = id.map(v => {
+  return { value: v, type: 'command' }
+}).skip(lparen).then(
+  id.skip(string('\'')).then(regex(/.[^\']*/)).skip(string('\'')).skip(rparen)
+)
+
+const expr = lazy('expr', () => ignore.then(
+  alt(
+    p1, p2
+
+
+  )
+))
+const command = ignore.then(expr.many())
 
 module.exports = {
   parse: (input) => {
+    // const lparen = string('(')
+    // const rparen = string(')')
+    //
+    // const expr = lazy('expression', () => {
+    //   return form.or(atom)
+    // })
+    //
+    // const number = lexeme(regex(/[0-9]+/)).map(parseInt)
+    // const id = lexeme(regex(/[a-z_]\w*/i))
+    //
+    // const atom = number.or(id)
+    //
+    // const form = lparen.then(expr.many()).skip(lparen)
+    // return alt(expr, ).parse(input)
+    console.log(input)
 
-    
+    const result = command.parse(input)
+    if (result.status === false) {
+      let indents = ''
+      let column = 0
+      let line = 1
+      for (let i = 0; i < result.index; i++) {
+        if (input[i] === '\n') {
+          indents = ''
+          column = 0
+          line += 1
+        } else {
+          indents += '~'
+          column += 1
+        }
+      }
+      console.log('\x1b[91m', '\nFAILURE: line: ' + line + ', column: ' + column+ '\n','\x1b[0m')
+      console.log(' ' + input.split('\n').slice(line - 3 > 0 ? line - 3 : 0, line).join('\n '))
+      console.log('\x1b[91m', indents + '^','\x1b[0m')
+      console.log(' ' + input.split('\n').slice(line, line + 3 <= input.length ? line + 3 : input.length).join('\n '))
+      console.log('\x1b[91m', `Got: '${input[result.index] ? input[result.index].replace('\n', '\\n'): 'EOF'}'. Expected: ${result.expected.join(' or ')}\n`,'\x1b[0m')
+    }
+    return result
   }
 }
--- src/test/shared/parser/index.js
+++ src/test/shared/parser/index.js
@@ -2,34 +2,74 @@ const Parser = require('src/main/shared/parser')
 
 const testUtils = (test) => {
   return {
-    equals: (a, b) => test.equals(a, b, a + ' != ' + b)
+    equals: (a, b) => test.deepEqual(a, b, JSON.stringify(a, null, 2) + ' != ' + JSON.stringify(b, null, 2))
   }
 }
 
-
 module.exports = {
     'basic parser eval': (test) => {
+
       const parser = Parser()
       const asts = {
-        'html1': [
-            {
-              symbol: 'cmd',
-              value: 'html',
-              type: 'Html',
-              params: [
-                {
-                  symbol: 'atom',
-
-                }
-              ]
-            }
-          ]
-        }
+        html1: [
+          {
+            type: 'expression',
+            value: 'html',
+            params: [{
+                type: 'expression',
+                value: 'h',
+                params: [{
+                  type: 'string',
+                  value: 'div',
+                  params: []
+                }, {
+                  type: 'string',
+                  value: 'foo',
+                  params: []
+                }]
+            }, {
+                type: 'expression',
+                value: 'h',
+                params: [{
+                  type: 'string',
+                  value: 'div',
+                  params: []
+                }, {
+                  type: 'expression',
+                  value: 'string',
+                  params: [{
+                    type: 'int',
+                    value: '1'
+                  }]
+                }]
+            }]
+          }
+        ]
       }
+      // testUtils(test).equals(parser.parse(`html (
+      // )`), asts.html1)
+      console.log(parser.parse(`#comment
+        html`))
+      console.log(parser.parse(`#comment
+          (h 'div')`))
+      console.log(parser.parse(`#comment
+          html (h 'div')`))
+      console.log(parser.parse(`#comment
+        html (
+          h 'div'
+        )`))
 
-      testUtils(test).equals(parser.parse(`html (
-        h 'div' 'foo'
-      )`), {})
+      // testUtils(test).equals(parser.parse(`html (
+      //   div '#Foo.Bar' [
+      //     div '.Zoo' 'hello'
+      //   ]
+      //   div '.Bar'
+      // )`), asts.html2)
+      //
+      //
+
+      test.done()
+    }
     //   testUtils(test).equals(parser.parse(`html (h 'div' 'foo')`), {})
     //   testUtils(test).equals(parser.parse(`html (
     //     h "div" 'foo'
@@ -67,4 +107,4 @@ module.exports = {
     //   ) )`), {})
     //   test.done()
     // }
-};
+}

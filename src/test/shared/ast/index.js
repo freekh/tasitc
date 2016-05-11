bf03f255d3f16bd1a85c390f40e60b4604b1a283
Fredrik Ekholdt <freekh@gmail.com>
2016-05-11 16:27:00 +0200

    They call me the believer

--- src/test/shared/ast/index.js
+++ src/test/shared/ast/index.js
@@ -3,7 +3,187 @@ module.exports = {
     //google/drive/Test.gsheet --acount=(account -l freekh) | gsheet2json | html ($.columns[0] | li)
     //lookup('google/drive/Test.gsheet', { account: lookup('account', {l: 'freekh'})}).then(res => lookup('gsheet2json', res).then(res => lookup('html', res.columns[0].then(lookup('li')))))
 
-    {
+// >>> print(astunparse.dump(ast.parse("[i.to_bytes(1, 'big') for i in range(10)]", mode='eval')))
+// Expression(body=ListComp(
+//   elt=Call(
+//     func=Attribute(
+//       value=Name(
+//         id='i',
+//         ctx=Load()),
+//       attr='to_bytes',
+//       ctx=Load()),
+//     args=[
+//       Num(n=1),
+//       Str(s='big')],
+//     keywords=[]),
+//   generators=[comprehension(
+//     target=Name(
+//       id='i',
+//       ctx=Store()),
+//     iter=Call(
+//       func=Name(
+//         id='range',
+//         ctx=Load()),
+//       args=[Num(n=10)],
+//       keywords=[]),
+//     ifs=[])]))
+
+// >>> print(astunparse.dump(ast.parse('[i**2 for i in range(10)]', mode='eval')))
+// Expression(body=ListComp(
+//   elt=BinOp(
+//     left=Name(
+//       id='i',
+//       ctx=Load()),
+//     op=Pow(),
+//     right=Num(n=2)),
+//   generators=[comprehension(
+//     target=Name(
+//       id='i',
+//       ctx=Store()),
+//     iter=Call(
+//       func=Name(
+//         id='range',
+//         ctx=Load()),
+//       args=[Num(n=10)],
+//       keywords=[]),
+//     ifs=[])]))
+
+// >>> print(astunparse.dump(ast.parse('[i*j for i in range(2) for j in range(3,5)]', mode='eval')))
+// Expression(body=ListComp(
+//   elt=BinOp(
+//     left=Name(
+//       id='i',
+//       ctx=Load()),
+//     op=Mult(),
+//     right=Name(
+//       id='j',
+//       ctx=Load())),
+//   generators=[
+//     comprehension(
+//       target=Name(
+//         id='i',
+//         ctx=Store()),
+//       iter=Call(
+//         func=Name(
+//           id='range',
+//           ctx=Load()),
+//         args=[Num(n=2)],
+//         keywords=[]),
+//       ifs=[]),
+//     comprehension(
+//       target=Name(
+//         id='j',
+//         ctx=Store()),
+//       iter=Call(
+//         func=Name(
+//           id='range',
+//           ctx=Load()),
+//         args=[
+//           Num(n=3),
+//           Num(n=5)],
+//         keywords=[]),
+//       ifs=[])]))
+
+// >>> print(astunparse.dump(ast.parse('[mult(i,j) for i in range(2) for j in range(3,5)]', mode='eval')))
+// Expression(body=ListComp(
+//   elt=Call(
+//     func=Name(
+//       id='mult',
+//       ctx=Load()),
+//     args=[
+//       Name(
+//         id='i',
+//         ctx=Load()),
+//       Name(
+//         id='j',
+//         ctx=Load())],
+//     keywords=[]),
+//   generators=[
+//     comprehension(
+//       target=Name(
+//         id='i',
+//         ctx=Store()),
+//       iter=Call(
+//         func=Name(
+//           id='range',
+//           ctx=Load()),
+//         args=[Num(n=2)],
+//         keywords=[]),
+//       ifs=[]),
+//     comprehension(
+//       target=Name(
+//         id='j',
+//         ctx=Store()),
+//       iter=Call(
+//         func=Name(
+//           id='range',
+//           ctx=Load()),
+//         args=[
+//           Num(n=3),
+//           Num(n=5)],
+//         keywords=[]),
+//       ifs=[])]))
+    
+// >>> print(astunparse.dump(ast.parse('mult(a = 1, b = 2)', mode='eval')))Expression(body=Call(
+//   func=Name(
+//     id='mult',
+//     ctx=Load()),
+//   args=[],
+//   keywords=[
+//     keyword(
+//       arg='a',
+//       value=Num(n=1)),
+//     keyword(
+//       arg='b',
+//       value=Num(n=2))]))
+
+// >>> print(astunparse.dump(ast.parse('[mult(i,j) for i in range(2) for j in range(3,5) if j > 1]', mode='eval')))
+// Expression(body=ListComp(
+//   elt=Call(
+//     func=Name(
+//       id='mult',
+//       ctx=Load()),
+//     args=[
+//       Name(
+//         id='i',
+//         ctx=Load()),
+//       Name(
+//         id='j',
+//         ctx=Load())],
+//     keywords=[]),
+//   generators=[
+//     comprehension(
+//       target=Name(
+//         id='i',
+//         ctx=Store()),
+//       iter=Call(
+//         func=Name(
+//           id='range',
+//           ctx=Load()),
+//         args=[Num(n=2)],
+//         keywords=[]),
+//       ifs=[]),
+//     comprehension(
+//       target=Name(
+//         id='j',
+//         ctx=Store()),
+//       iter=Call(
+//         func=Name(
+//           id='range',
+//           ctx=Load()),
+//         args=[
+//           Num(n=3),
+//           Num(n=5)],
+//         keywords=[]),
+//       ifs=[Compare(
+//         left=Name(
+//           id='j',
+//           ctx=Load()),
+//         ops=[Gt()],
+//         comparators=[Num(n=1)])])]))
+
+    
+    const ast = {
       type: 'Cmd',
       value: 'google/drive/Test.gsheet',
       args: [
@@ -43,15 +223,21 @@ module.exports = {
               value: {
                 type: 'Dot',
                 value: {
-
-                },
-
+                  
+                }
               }
             }
           ]
         }
       }
-
     }
+
+    const transpile = (ast) => {
+      if (ast.type === 'Cmd') {
+        
+      } else {
+        
+      }
+    }    
   }
 }

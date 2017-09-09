e7a4d581bef4f16fa502ad2fe69cd983bbae4722
Fredrik Ekholdt <freekh@gmail.com>
2017-09-09 21:12:57 +0200

    Young butler

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -124,28 +124,6 @@ fn token_type(c: char) -> TokenType  {
   }
 }
 
-macro_rules! reset {
-  ( $tokens: expr, $peek_prev_type: expr, $last_token: expr, $pos: expr, $line: expr, $column: expr) => {
-    $peek_prev_type = None;
-    $last_token = Token {
-      token_type: TokenType::New,
-      pos: Pos {
-        file: $pos,
-        line: $line,
-        column: $column,
-      },
-      content: None,
-    };
-  }
-}
-
-macro_rules! bump {
-  ( $c: expr, $peek_prev_type: expr, $last_token: expr, $pos: expr, $line: expr, $column: expr) => {
-    $last_token.push_content($c, $pos, $line, $column);
-    $peek_prev_type = Some(token_type($c));
-  }
-}
-
 fn valid_sue_token(token_type: TokenType) -> bool {
   token_type == TokenType::Sue || 
   token_type == TokenType::Dot || 
@@ -172,43 +150,20 @@ pub fn tokenize(input: &str) -> Vec<Token> { // TODO: Vec => Iterator
   let mut peek_prev_type: Option<TokenType> = None;
 
   for (pos, c) in input.chars().enumerate() {
-    let token_type = token_type(c);
-    match (token_type.clone(), peek_prev_type.clone(), last_token.token_type.clone()) {
-      // Slurp Doc
-      (_,  Some(TokenType::Tar), TokenType::Doc) => { // doc slurp ends
-        if token_type == TokenType::Hax {
-          tokens.push(last_token);
-          reset!(tokens, peek_prev_type, last_token, pos, line, column);
-        } else {
-          bump!(c, peek_prev_type, last_token, pos, line, column);
-        }
-      }
-      (_, _, TokenType::Doc) => {
-        bump!(c, peek_prev_type, last_token, pos, line, column);
-      }
-      (TokenType::Tar, _, TokenType::Hax) => { // doc slurp starts
-        last_token = Token {
-          pos: last_token.pos,
+    macro_rules! token {
+      ( $token_type: expr) => {
+        Token {
+          pos: Pos {
+            file: pos, 
+            line, column
+          },
           content: None,
-          token_type: TokenType::Doc,
+          token_type: $token_type,
         };
       }
-      // Slurp Hax (comment)
-      (TokenType::New, _, TokenType::Hax) => { // hax slurp ends
-        tokens.push(last_token);
-        reset!(tokens, peek_prev_type, last_token, pos, line, column);
-      }
-      (_, _, TokenType::Hax) => {
-        bump!(c, peek_prev_type, last_token, pos, line, column);
-      }
-      // Escapes
-      (TokenType::Bas, Some(TokenType::Bas), _) => { // escape escape
-        last_token.push_content(c, pos, line, column);
-        peek_prev_type = None;
-      }
-      (TokenType::Bas, _, _) => { // escape
-        peek_prev_type = Some(TokenType::Bas);
-      }
+    }
+    let token_type = token_type(c);
+    match (token_type.clone(), peek_prev_type.clone(), last_token.token_type.clone()) {
       // Slurp Soq
       (TokenType::Soq, Some(TokenType::Bas), TokenType::Soq) => { // ignore escape
         last_token.push_content(c, pos, line, column);
@@ -216,14 +171,7 @@ pub fn tokenize(input: &str) -> Vec<Token> { // TODO: Vec => Iterator
       }
       (TokenType::Soq, _, TokenType::Soq) => {
         tokens.push(last_token.clone());
-        last_token = Token {
-          pos: Pos {
-            file: pos, 
-            line, column
-          },
-          content: None,
-          token_type: TokenType::New,
-        };
+        last_token = token!(TokenType::New);
       }      
       (_, _, TokenType::Soq) => {
         last_token.push_content(c, pos, line, column);
@@ -235,17 +183,55 @@ pub fn tokenize(input: &str) -> Vec<Token> { // TODO: Vec => Iterator
       }
       (TokenType::Doq, _, TokenType::Doq) => {
         tokens.push(last_token.clone());
-        last_token = Token {
-          pos: Pos {
-            file: pos, 
-            line, column
-          },
-          content: None,
-          token_type: TokenType::New,
-        };
+        last_token = token!(TokenType::New);
       }      
       (_, _, TokenType::Doq) => {
         last_token.push_content(c, pos, line, column);
+      }
+      // Slurp Doc
+      (TokenType::New, None, TokenType::Doc) => { // doc slurp pushed
+        tokens.push(last_token.clone());
+        last_token = token!(TokenType::New);
+      }
+      (TokenType::Hax, Some(TokenType::Tar), TokenType::Doc) => { // doc slurp after
+        last_token.push_content(c, pos, line, column);
+        peek_prev_type = None;
+      }
+      (_, _, TokenType::Doc) => {
+        last_token.push_content(c, pos, line, column);
+        peek_prev_type = Some(token_type);
+      }
+      (TokenType::Tar, _, TokenType::Hax) => { // doc slurp starts
+        last_token = Token {
+          pos: last_token.pos,
+          content: last_token.content,
+          token_type: TokenType::Doc,
+        };
+        last_token.push_content(c, pos, line, column);
+        peek_prev_type = Some(token_type);
+      }
+      // Slurp Hax (comment)
+      (TokenType::New, _, TokenType::Hax) => { // hax slurp ends
+        tokens.push(last_token);
+        peek_prev_type = None;
+        last_token = token!(TokenType::New);
+      }
+      (TokenType::Hax, _, _) => {
+        last_token = token!(TokenType::Hax);
+        peek_prev_type = Some(token_type);
+        last_token.push_content(c, pos, line, column);
+      }
+      (_, _, TokenType::Hax) => {
+        last_token.push_content(c, pos, line, column);
+        peek_prev_type = Some(token_type);
+      }
+      // Escapes
+      (TokenType::Bas, Some(TokenType::Bas), _) => { // escape escape
+        last_token.push_content(c, pos, line, column);
+        peek_prev_type = None;
+      }
+      (TokenType::Bas, _, _) => { // escape
+        peek_prev_type = Some(TokenType::Bas);
       }
       // Slurps Num
       (TokenType::Dot, _, TokenType::Int) => {
@@ -265,14 +251,7 @@ pub fn tokenize(input: &str) -> Vec<Token> { // TODO: Vec => Iterator
       }
       (TokenType::Int, _, _) => {
         tokens.push(last_token.clone());
-        last_token = Token {
-          pos: Pos {
-            file: pos, 
-            line, column
-          },
-          content: None,
-          token_type: token_type,
-        };
+        last_token = token!(token_type);
         last_token.push_content(c, pos, line, column);
       }
       // Slurps Sue
@@ -281,39 +260,19 @@ pub fn tokenize(input: &str) -> Vec<Token> { // TODO: Vec => Iterator
           last_token.push_content(c, pos, line, column);
         } else {
           tokens.push(last_token.clone());
-          last_token = Token {
-            pos: Pos {
-              file: pos, 
-              line, column
-            },
-            content: None,
-            token_type: token_type,
-          };
+          last_token = token!(token_type);
         }
       }
       (TokenType::Sue, _, _) => {
         tokens.push(last_token.clone());
-        last_token = Token {
-          pos: Pos {
-            file: pos, 
-            line, column
-          },
-          content: None,
-          token_type: token_type,
-        };
+        last_token = token!(token_type);
         last_token.push_content(c, pos, line, column);
       }
       //
       _ => {
         tokens.push(last_token);
-        last_token = Token {
-          pos: Pos {
-            file: pos, 
-            line, column
-          },
-          content: None,
-          token_type: token_type,
-        };
+        peek_prev_type = None;
+        last_token = token!(token_type);
       }
     }
     if c == '\n' {
@@ -324,6 +283,5 @@ pub fn tokenize(input: &str) -> Vec<Token> { // TODO: Vec => Iterator
     }
   }
   tokens.push(last_token);
-  tokens.remove(0);
   tokens
 }

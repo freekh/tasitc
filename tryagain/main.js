7092358e0609c3c236109e854ab1d77ead93660d
Fredrik Ekholdt <freekh@gmail.com>
2017-10-20 13:25:25 +0200

    No pretending

--- tryagain/main.js
+++ tryagain/main.js
@@ -17,14 +17,13 @@ const rules = {
       { v: "a" },
       { v: "b" },
       { v: "c" },
-      // { v: "d" },
+      { v: "d" },
       // { v: "e" },
       // { v: "f" },
       // { v: "g" },
     ],
   },
   "obj_val": {
-    intransitive: true,
     e: [
       { r: "sue", k: "lhs" },
       { v: ":" },
@@ -38,30 +37,22 @@ const rules = {
     ],
   },
   "obj": {
-    intransitive: true,
     e: [
       { v: "{" },
-      { r: "obj_val" },
-      // {
-      //   star: true,
-      //   a: [
-      //     { r: "obj_val" },
-      //     {
-      //       e: [
-      //         { r: "obj_val" },
-      //         { v: "," },
-      //       ],
-      //     },
-      //   ]
-      // },
+      {
+        plus: true,
+        e: [
+          { r: "obj_val" },
+          { v: "," },
+        ],
+      },
       { v: "}" },
     ],
   },
 };
 
 // const input = "{a:{b:c},{d:e,f:g}}}";
-const input = "{a:b,c:d}";
-
+const input = "{a:b,c:d,}";
 
 const exec = (rules, input) => {
   const iti = (rule, cursor) => {
@@ -75,7 +66,16 @@ const exec = (rules, input) => {
           match = false;
           break;
         } else {
-          next_cursor = result.cursor;
+          let plus_result = result;
+          while (then_rule.plus) {
+            const result = iti(then_rule, plus_result.cursor);
+            if (result.match) {
+              plus_result = result;
+            } else {
+              break;
+            }
+          }
+          next_cursor = plus_result.cursor;          
         }
       }
       return { match, cursor: next_cursor };
@@ -88,7 +88,7 @@ const exec = (rules, input) => {
           return { match: true, cursor: result.cursor };
         }
       }
-      return { match: false, cursor };
+      return { match: true, cursor };
     } else if (rule.v) {
       const match = token === rule.v;
       return { match, cursor: cursor + 1 };
@@ -98,16 +98,16 @@ const exec = (rules, input) => {
   return iti(rules.obj, 0);
 };
 
-const { match, cursor } = exec(rules, input);
-if (!match) {
-  console.log('ERROR: unexpected token');
+const result = exec(rules, input);
+if (!result.match) {
+  console.log('ERROR: unexpected token', result);
   console.log(input);
-  for (let i = 0; i < cursor; i++) {
+  for (let i = 0; i < result.cursor; i++) {
     process.stdout.write(" ");
   }
   process.stdout.write("^\n");
 } else {
-  console.log(input);
+  console.log(result);
 }
 
 

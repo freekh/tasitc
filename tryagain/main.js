0ebd41f2f8869db971c4aaeb3d5e4ae99ab7e86a
Fredrik Ekholdt <freekh@gmail.com>
2017-10-31 16:18:11 +0100

    this will be used directly

--- tryagain/main.js
+++ tryagain/main.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { parse } = require('./parser');
+const { BloomFilter } = require('bloomfilter');
 
 // Hack
 // const stdin = fs.readFileSync(0).toString();
@@ -24,76 +25,33 @@ const rules = {
     ],
   },
   "obj_val": {
-    // e: [
-    //   { r: "sue", k: "lhs" },
-    //   { v: ":" },
-    //   {
-    //     k: "rhs",
-    //     a: [
-    //       { r: "sue" },
-    //       { r: "obj" },
-    //     ],
-    //   },
-    // ],
-    a: [
+    e: [
+      { r: "sue", k: "lhs" },
+      { v: ":" },
       {
-        e: [
-          { r: "sue", k: "lhs" },
-          { v: ":" },
-          {
-            k: "rhs",
-            r: "sue" ,
-          }
+        k: "rhs",
+        a: [
+          { r: "sue" },
+          { r: "obj" },
         ],
       },
-      {
-        e: [
-          { r: "sue", k: "lhs" },
-          { v: ":" },
-          {
-            k: "rhs",
-            r: "obj" ,
-          }
-        ],
-      }
     ],
   },
   "obj": {
-    // e: [
-    //   { v: "{" },
-    //   {
-    //     a: [
-    //       {
-    //         e: [
-    //           { r: "obj_val" },
-    //           { v: "," },
-    //         ],
-    //       },
-    //       { r: "obj_val" },
-    //     ],
-    //   },
-    //   { v: "}" },
-    // ],
-    a: [
+    e: [
+      { v: "{" },
       {
-        e: [
-          { v: "{" },
-          { r: "obj_val" },          
-          { v: "}" },
-        ],
-      },
-      {
-        e: [
-          { v: "{" },
+        a: [
+          { r: "obj_val" },
           {
             e: [
               { r: "obj_val" },
               { v: "," },
             ],
           },
-          { v: "}" },
         ],
       },
+      { v: "}" },
     ],
   },
 };
@@ -101,51 +59,65 @@ const rules = {
 // const input = "{a:{b:c},{d:e,f:g}}}";
 const input = "{a:b,}";
 
-const exec = (rules, input) => {
-  const iti = (rule, cursor) => {
+const exec = (rules, input) => (start) => {
+  const trails = new BloomFilter(32 * 256, 16);
+
+  const iti = (rule, cursor, id, trail, complete) => {
+    trails.add(trail.join(''));
     const token = input[cursor];
     if (rule.e) {
-      let next_cursor = cursor;
-      let match = true;
+      let next_result = { cursor, id, trail: trail.concat(id + '[' + '0' + ']') };
+      let i = 0;
       for (const then_rule of rule.e) {
-        const result = iti(then_rule, next_cursor);
-        if (!result.match) {
-          match = false;
+        next_result = iti(then_rule, next_result.cursor, id + '[' + i + ']', next_result.trail, complete);
+        if (!next_result.match) {
           break;
-        } else {
-          next_cursor = result.cursor;          
         }
+        i++;
       }
-      return { match, cursor: next_cursor };
+      if (!next_result.match && !next_result.complete) {
+        return iti(rule, cursor, id, trail, complete);
+      } else {
+        return next_result;
+      }
     } else if (rule.r) {
-      return iti(rules[rule.r], cursor);
+      return iti(rules[rule.r], cursor, rule.r, trail);
     } else if (rule.a) {
+      let i = 0;
+      let skipped = 0;
       for (const alt_rule of rule.a) {
-        const result = iti(alt_rule, cursor);
-        if (result.match) {
-          return { match: true, cursor: result.cursor };
+        const alt_trail = trail.concat(id + '(' + i + ')');
+        const not_visited = trails.test(alt_trail.join('')) === false; // false if definitely not visited
+        if (not_visited) {
+          const result = iti(alt_rule, cursor, id, alt_trail, complete);
+          if (result.match) {
+            return result;
+          }
+        } else {
+          skipped++;
         }
+        i++;
       }
-      return { match: false, cursor };
+      return { match: false, cursor, complete: skipped === rule.a.length && complete, id, trail };
     } else if (rule.v) {
       const match = token === rule.v;
-      return { match, cursor: cursor + 1 };
+      return { match, cursor: cursor + 1, token, id, trail: trail.concat("token:'"+token+"'") };
     }
     throw new Error(`Malformed rule: ${ JSON.stringify(rule) }`);
   };
-  return iti(rules.obj, 0);
+  return iti(rules[start], 0, start, [], false);
 };
 
-const result = exec(rules, input);
+const result = exec(rules, input)("obj");
 if (!result.match) {
-  console.log('ERROR: unexpected token', result);
+  console.log('ERROR: unexpected token', JSON.stringify(result, null, 2));
   console.log(input);
   for (let i = 0; i < result.cursor; i++) {
     process.stdout.write(" ");
   }
   process.stdout.write("^\n");
 } else {
-  console.log(result);
+  console.log(JSON.stringify(result, null, 2));
 }
 
 
